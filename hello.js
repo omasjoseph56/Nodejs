38d407633a550e0ad520341a7ce00f63b6b75174
Joseph Omas <josephomas6@gmail.com>
2021-10-10 19:27:46 +0800

    added procfile

--- hello.js
+++ hello.js
@@ -1,6 +1,7 @@
 var http= require('http');
 var url = require('url');
 var fs = require('fs');
+const port = process.env.PORT || 5000
 http.createServer(function(req, res){
     var q = url.parse(req.url, true);
     console.log(q);
@@ -15,4 +16,4 @@ http.createServer(function(req, res){
         res.end();
     });
     
-}).listen(8181);
+}).listen(port);
